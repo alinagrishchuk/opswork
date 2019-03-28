9532e6ca94dc4fed82e84e9eeae484b617342aa6
Alina Grishchuk <alina.grishchuk91@gmail.com>
2019-03-28 14:46:23 +0200

    test

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures nginx'
 version '0.1.0'
 chef_version '>= 13.0'
 
-depends "apt"
+# depends "apt"
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
