59fd23eeb7022c52ea45fe3fae3477dd8996ce56
Alina Grishchuk <alina.grishchuk91@gmail.com>
2019-03-28 14:59:33 +0200

    test

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -1,21 +1,20 @@
-name 'nginx'
+name 'test'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures nginx'
-long_description 'Installs/Configures nginx'
-# version '0.1.0'
-# chef_version '>= 13.0'
+description 'Installs/Configures test'
+long_description 'Installs/Configures test'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 
-# depends "apt"
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/nginx/issues'
+# issues_url 'https://github.com/<insert_org_here>/test/issues'
 
-# The `source_url` points to the development repository for this cookbook.  A
+# The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/nginx'
+# source_url 'https://github.com/<insert_org_here>/test'
